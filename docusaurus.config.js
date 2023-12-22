e37ea40dd91a454b9e7b72aa3786832da73958aa
Joshua <joshdcannon@gmail.com>
2023-12-22 11:37:15 -0600

    baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   url: "https://pantsbuild.github.io",
-  baseUrl: "/",
+  baseUrl: "/pantsbuild.org",
   trailingSlash: false,
 
   organizationName: "pantsbuild",
