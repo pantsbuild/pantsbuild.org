5c248ae2a65f8b13cd783ad89aa5bc2be86b2614
Joshua <joshdcannon@gmail.com>
2023-12-27 21:38:21 -0600

    its beautfil

--- src/components/reference/Field.jsx
+++ src/components/reference/Field.jsx
@@ -1,29 +1,28 @@
-export default function Field({
-  children,
-  type_repr,
-  default_repr,
-  backend
-}) {
+export default function Field({ children, type_repr, default_repr, backend }) {
   return (
     <div className="margin-bottom--lg">
-      <code style={{ color: "var(--pants-reference-option-repr)"}}>{type_repr}</code>
-      <br/>
+      <code style={{ color: "var(--pants-reference-option-repr)" }}>
+        {type_repr}
+      </code>
+      <br />
       <div style={{ paddingLeft: "2em" }}>
-        {default_repr &&
+        {default_repr && (
           <span>
-            default: <code style={{ color: "var(--ifm-color-success)" }}>{default_repr}</code>
+            default:{" "}
+            <code style={{ color: "var(--ifm-color-success)" }}>
+              {default_repr}
+            </code>
           </span>
-        }
-        {
-          !default_repr &&
+        )}
+        {!default_repr && (
           <span style={{ color: "var(--ifm-color-success)" }}>required</span>
-        }
-        {
-          backend &&
+        )}
+        {backend && (
           <div>
-            backend: <code style={{ color: "var(--ifm-color-success)" }}>{backend}</code>
+            backend:{" "}
+            <code style={{ color: "var(--ifm-color-success)" }}>{backend}</code>
           </div>
-        }
+        )}
 
         {children}
       </div>
