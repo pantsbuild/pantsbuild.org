2e423c5b4882995046c813ef6fab50291f23d905
Worker Pants (Pantsbuild GitHub Automation Bot) <pantsbuild+github-automation@gmail.com>
2023-12-28 03:34:22 +0000

    Update docs site for version 2.18.2rc0

--- src/components/reference/Option.jsx
+++ src/components/reference/Option.jsx
@@ -9,36 +9,43 @@ export default function Option({
 }) {
   return (
     <div className="margin-bottom--lg">
-      <code style={{ color: "var(--pants-reference-option-repr)"}}>{cli_repr}</code>
-      <br/>
-      <code style={{ color: "var(--pants-reference-option-repr)" }}>{env_repr}</code>
+      <code style={{ color: "var(--pants-reference-option-repr)" }}>
+        {cli_repr}
+      </code>
+      <br />
+      <code style={{ color: "var(--pants-reference-option-repr)" }}>
+        {env_repr}
+      </code>
       <br />
       <div style={{ paddingLeft: "2em" }}>
         <span>
-          {
-            one_of &&
-              <span >
-                one of: <code style={{ color: "var(--ifm-color-success)" }}>{one_of}</code>
-                <br />
-              </span>
-          }
-          default: <span style={{ color: "var(--ifm-color-success)" }}>
-            {
-              (default_repr || "").includes("\n")
-                ? <pre className="padding--sm">{default_repr}</pre>
-                : <code>{default_repr}</code>
-            }
+          {one_of && (
+            <span>
+              one of:{" "}
+              <code style={{ color: "var(--ifm-color-success)" }}>
+                {one_of}
+              </code>
+              <br />
+            </span>
+          )}
+          default:{" "}
+          <span style={{ color: "var(--ifm-color-success)" }}>
+            {(default_repr || "").includes("\n") ? (
+              <pre className="padding--sm">{default_repr}</pre>
+            ) : (
+              <code>{default_repr}</code>
+            )}
           </span>
         </span>
         <br />
-        {removal_version &&
+        {removal_version && (
           <span style={{ color: "var(--ifm-color-danger-darkest)" }}>
             Deprecated, will be removed in version: {removal_version}.
             <br />
             {removal_hint}
             <br />
           </span>
-        }
+        )}
         {children}
       </div>
     </div>
