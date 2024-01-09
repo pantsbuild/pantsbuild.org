efbf73eb84bf24487b0875a4f0c095cee157ed3d
Joshua <joshdcannon@gmail.com>
2024-01-08 20:18:04 -0600

    Add comment

--- src/js/redirectCodeFragment.js
+++ src/js/redirectCodeFragment.js
@@ -1,4 +1,8 @@
 export function onRouteUpdate({ location, previousLocation }) {
+  // NB: Redirect fragments on reference pages if they look like `code{id}code`.
+  //  This only exists because the old readme.com docs used `<code>{id}</code>` in the header
+  //  and readme.com turned that into `id="code{id}code"`. So to continue to support those links,
+  //  we redirect.
   if (location.hash && location.pathname.includes("/reference/")) {
     const matches = /^#code(.+)code$/.exec(location.hash);
     if (matches) {
