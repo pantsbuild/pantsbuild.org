f4ab270698611ce6f5c7fb37a3183e114e383c74
Tom Solberg <me@sbg.dev>
2024-01-28 20:32:11 +0100

    use regex and check `help-all.json`

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,6 +2,9 @@ import versions from "./versions.json";
 import redirects from "./old_site_redirects.js";
 import captionedCode from "./src/remark/captioned-code.js";
 import tabBlocks from "docusaurus-remark-plugin-tab-blocks";
+import fs from "fs";
+import fsPromises from "fs/promises";
+import path from "path";
 
 import { themes as prismThemes } from "prism-react-renderer";
 
@@ -45,6 +48,30 @@ const formatCopyright = () => {
     : "local";
 
   return `Copyright © Pants project contributors. ${repoLink} @ ${commitLink}.`;
+};
+
+const isPrerelease = (version) => {
+  const reference_dir = path.join(
+    "versioned_docs",
+    `version-${version}`,
+    "reference"
+  );
+  const helpAll = JSON.parse(
+    fs.readFileSync(path.join(reference_dir, "help-all.json"), "utf8")
+  );
+
+  const pantsVersion = helpAll["scope_to_help_info"][""]["advanced"].find(
+    (help) => help["config_key"] === "pants_version"
+  );
+
+  const hardcoded = pantsVersion["value_history"]["ranked_values"].find(
+    (value) => value["rank"] == "HARDCODED"
+  );
+
+  // Check if it's one of xx.xx.xx.dev0, xx.xx.xxa0, xx.xx.xxrc0
+  const rex = /^(\d+\.\d+\.\d+)(\.dev|a|rc)\d+$/;
+
+  return rex.test(hardcoded["value"]);
 };
 
 const config = {
@@ -310,18 +337,16 @@ const config = {
             ? {}
             : versions.reduce((acc, version, index) => {
                 acc[version] = {
-                  label:
-                    index === 0
-                      ? `${version}`
-                      : index < 3
-                        ? version
-                        : `${version} (deprecated)`,
-                  banner:
-                    index == 0
-                      ? "unreleased"
-                      : index < 3
-                        ? "none"
-                        : "unmaintained",
+                  label: isPrerelease(version)
+                    ? `${version} (prerelease)`
+                    : index < 3
+                      ? version
+                      : `${version} (deprecated)`,
+                  banner: isPrerelease(version)
+                    ? "unreleased"
+                    : index < 3
+                      ? "none"
+                      : "unmaintained",
                   noIndex: index >= 3,
                   path: version,
                 };
