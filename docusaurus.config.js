51a4cebc5252c4d274d6250d7e9ece8805f191ee
Tom Solberg <me@sbg.dev>
2024-01-30 09:33:30 +0100

    check prerelease for lastVersion

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -327,7 +327,7 @@ const config = {
         routeBasePath: "/",
         disableVersioning,
         onlyIncludeVersions,
-        lastVersion: onlyIncludeVersions ? undefined : versions[1],
+        lastVersion: onlyIncludeVersions ? undefined : versions.find((v) => !isPrerelease(v)),
         versions: {
           current: {
             label: `${currentVersion} (dev)`,
