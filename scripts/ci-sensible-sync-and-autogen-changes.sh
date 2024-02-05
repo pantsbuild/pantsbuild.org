8206bd334766ad010ccd143d2faf6abb0b244eb7
Huon Wilson <wilson.huon@gmail.com>
2024-02-05 13:22:03 +1100

    Add PR CI check that humans don't edit auto-generated files (#104)
    This fixes #76 by validating that only PRs that have the
    `automation:sync-docs` label make changes to the files that are directly
    from the pantsbuild/pants repo, in the `docs/` and `versioned_docs/*`
    directories:
    
    - prose docs in `docs/`
    - help JSON in `reference/help-all.json`
    
    Generally speaking, a human shouldn't change these files directly. The
    sync docs automation attaches the label, and if a human knows what
    they're doing, they can add the label themselves.
    
    This is an "MVP" approach, checking if there's changes and outputting a
    vaguely explanatory message. Future iterations might comment directly on
    the PR and/or construct links about which file(s) need to be updated,
    but that's a bit fiddly.
    
    This PR also updates the error about the reference docs, in case someone
    edits those files directly too.
    
    Examples: 
    
    - error when there's an inappropriate change:
    https://github.com/pantsbuild/pantsbuild.org/actions/runs/7534490860/job/20508964554
    - no error when there's a change with the `automation:sync-docs` label:
    https://github.com/pantsbuild/pantsbuild.org/actions/runs/7534631863/job/20509314384
    - (regression test) still an error when the reference auto-generated
    files are manually changed:
    https://github.com/pantsbuild/pantsbuild.org/actions/runs/7534496644/job/20508980096

---
+++ scripts/ci-sensible-sync-and-autogen-changes.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+echo "*** Checking files synced from pantsbuild/pants ***"
+has_label_automation_sync_docs="$1"
+
+# if the label exists, any changes are fine, so no need to check
+if [[ ${has_label_automation_sync_docs} != true ]]; then
+    synced=(
+        docs/docs
+        docs/reference/help-all.json
+        versioned_docs/*/docs
+        versioned_docs/*/reference/help-all.json
+    )
+
+    git fetch --depth=1 origin "${GITHUB_BASE_REF}"
+
+    if ! git diff --quiet FETCH_HEAD HEAD -- "${synced[@]}"; then
+        echo "::group::Full diff"
+        git diff FETCH_HEAD HEAD -- "${synced[@]}"
+        echo "::endgroup::"
+
+        cat <<EOF
+Error: found changes to files that are synced from <https://github.com/pantsbuild/pants>:
+
+$(git diff --name-only FETCH_HEAD HEAD -- "${synced[@]}")
+
+Those files should be changed in <https://github.com/pantsbuild/pants>, and are
+automatically synced to this repository after each release.
+
+(If this change is intentional and you're sure it's appropriate, add the
+'automation:sync-docs' label.)
+EOF
+
+        exit 1
+    else
+        echo "No changes, all good!"
+    fi
+else
+    echo "Labelled with automation:sync-docs, so any changes are fine"
+fi
+
+echo "*** Checking generated reference files are up-to-date ***"
+# NB. in future, it would be nice for this to be packaged as a test that one can run
+# locally, e.g. https://github.com/pantsbuild/pants/discussions/18235
+npm run generate-reference-all
+
+if ! git diff --quiet ; then
+    echo "::group::Full diff"
+    git diff
+    echo "::endgroup::"
+
+    cat <<EOF
+Error: the references have differences between what's committed and what would be
+generated. Either:
+
+- if you made changes to the generation scripts, please regenerate with 'npm run
+  generate-reference-all' or 'npm run generate-reference <specific directory>'.
+
+- if you have edited the files directly, https://github.com/pantsbuild/pants is the
+  source of truth: make the changes there and they will be synced here on the next
+  release.
+EOF
+
+    exit 1
+else
+    echo "No changes, all good!"
+fi
